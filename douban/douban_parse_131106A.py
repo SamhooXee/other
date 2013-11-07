62c2b73954d23a46a964797b39af2ff600d35cfe
Samhoo Xee <wariosunshine@gmail.com>
2013-11-07 22:53:01 +0800

    some cleanup

--- douban/douban_parse_131106A.py
+++ douban/douban_parse_131106A.py
@@ -1,23 +1,7 @@
 __author__ = 'abc'
 
-import HTMLParser
 from pyquery import PyQuery as pq
-# from lxml import etree
 import datetime
-
-class MyParser(HTMLParser.HTMLParser):
-    def __init__(self):
-        HTMLParser.HTMLParser.__init__(self)
-
-    def handle_starttag(self, tag, attrs):
-        if tag == 'li':
-            try:
-                for name,value in attrs:
-                    # if name == 'href':
-                    print value
-            except:
-                print 'error onece'
-
 
 def removeScript(input, output):
     with open(input) as f1:
@@ -45,11 +29,6 @@ def removeScript(input, output):
                     if scriptFlag==0:
                         f2.write(eachline)
 
-def printValue(v, x):
-    print v
-    print type(x)
-    print x.text
-
 exportFile = 'doubanMyMovie/output'
 outputFile = 'doubanMyMovie/tmp'
 
@@ -58,51 +37,28 @@ if __name__ == '__main__':
     for i in range(1282/30+1):
         filenName = '%s_%02d' % (exportFile, i)
         removeScript(filenName, outputFile)
-        # with open('outputNoscript.html') as f:
-        #     a = f.read()
-        # my = MyParser()
-        # my.feed(a)
         with open(outputFile) as f:
             a = f.read()
-            # d = pq(filename='outputNoscript.html')
             d = pq(a)
 
             p = d('.item-show')
             # print len(p)
-            # p.each(printValue)
-            # p.each(lambda e: e('.title a').text())
             for each in p:
                 items = each.findall('div')
                 OneMovie = {}
                 for item in items:
-                    # print type(item)
-                    # print item.attrib
                     if item.attrib['class'] == 'title':
                         href = item.find('a').attrib['href']
                         title = item.find('a').text.strip().replace(',', '_', 100)
                         OneMovie['title'] = title
                         OneMovie['href'] = href
                         pass
-                    # still problem
                     if item.attrib['class'] == 'date':
                         date = item.find('span').tail.strip()
                         OneMovie['date'] = date
-                        # print item.text.strip()
                         pass
-                # print '%s, %s, %s' % (OneMovie['date'], OneMovie['title'], OneMovie['href'])
+                print '%s, %s, %s' % (OneMovie['date'], OneMovie['title'], OneMovie['href'])
                 MovieList.append(OneMovie)
-
-            # titles = d('.item-show .title a')
-            # dates = d('.item-show .date')
-            # for each in titles:
-            #     print each.text.strip()
-            # for each in dates:
-            #     print each.text
-
-            # print titles.text()
-            # print len(titles)
-            # print dates.text()
-            # print len(dates)
     cnt2010 = {}
     cnt2011 = {}
     cnt2012 = {}
